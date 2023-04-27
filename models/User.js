3a81646b146e5e91c6c6158a68e63046ade612f5
Vatsal Zinzuvadiya <prajapativatsal1072@gmail.com>
2023-04-27 22:32:30 +0530

    first commit

---
+++ models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+    {
+        firstName : {
+            type:String,
+            require:true,
+            min:2,
+            max:50,
+        },
+        lastName : {
+            type:String,
+            require:true,
+            min:2,
+            max:50,
+        },
+        email : {
+            type:String,
+            require:true,
+            max:50,
+            unique:true
+        },
+        passWord : {
+            type:String,
+            require:true,
+            min:5
+        }, 
+        picturePath : {
+            type:String,
+            default: []
+        },
+        location:String,
+        occupation:String,
+        viewProfile:Number,
+        impressions:Number,
+    }, 
+    {timestamps: true}
+)
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
---
+++ routes/auth.js
@@ -0,0 +1,9 @@
+import { Express } from "express";
+import {login} from  "../controllers/auth.js"
+
+const router = express.Router();
+
+router.post("/login" ,login);   
+
+
+export default router; 
