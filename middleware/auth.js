718cc5182c56c2aaa91e050e9d6eaeb5f9757e04
Vatsal Zinzuvadiya <prajapativatsal1072@gmail.com>
2023-04-28 22:18:19 +0530

    Back-end completed

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = async  (res, req , next) => {
+    try{
+        let token = req.header("Authorization");
+
+        if(!token) {
+            return res.status(403).send("Access denied")
+        }
+
+        if(token.startWith("Berrer ")) {
+            token = token.slice(7, token.length).trimLeft()
+        }
+
+        const verified = jwt.verify(token , process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+
+    } catch(err) {
+        res.status(500).json({ error : err.message});   
+    }
+}
---
+++ routes/posts.js
@@ -0,0 +1,18 @@
+import express, { Router } from "express";
+import {getFeedPosts , getUserPost , likePost} from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js"
+
+const router = express.Router();
+
+/* READ */
+router.get("/" , verifyToken , getFeedPosts);
+router.get("/:userId/posts" , verifyToken , getUserPost);
+
+
+/* UPDATE */
+
+router.patch("/:id/like" , verifyToken , likePost);
+
+
+export default router;
+
